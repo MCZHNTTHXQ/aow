219a37c1403ceb7319954ad999132347185f96c5
MCZHNTTHXQ <1418271449@qq.com>
2018-04-23 19:19:41 +0800

    tj 测试

---
+++ app/Praise.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Praise extends Model
+{
+    /**
+     * 关联到模型的数据表
+     *
+     * @var string
+     */
+    protected $table = 'aow_praise';
+}
